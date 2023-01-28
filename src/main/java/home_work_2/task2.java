5bd3f4699a686ca7f5d65387ec6c501f59c21237
GromGT12 <m.krymov@wp.pl>
2023-01-28 16:08:12 +0100

    Fixes

---
+++ src/main/java/home_work_2/task2.java
@@ -0,0 +1,19 @@
+package home_work_2;
+
+public class task2 {
+    public static void main(String[] args) {
+
+        int i = Integer.parseInt("-78945627");
+        double d = Double.parseDouble("-8965.876655");
+        Short s = Short.parseShort("12587");
+        Byte b = Byte.parseByte("77");
+        Float f = Float.parseFloat("123.6789");
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(s);
+        System.out.println(b);
+        System.out.println(f);
+    }
+}
+
