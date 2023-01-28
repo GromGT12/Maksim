790308ea4b657a5404d7196ef19423b48a0b0752
Pavel Radkevich <40318685+RadkevichP@users.noreply.github.com>
2023-01-28 14:31:36 +0200

    Проверка1

--- src/main/java/Home_work/Maks_12.java
+++ src/main/java/Home_work/Maks_12.java
@@ -1,5 +1,8 @@
 package Home_work;
 
+// Не размещай несколько классов в одном файле. Лучше раздели на отдельные классы Task4, Task5, Task...
+// Во всех классах сделай форматирование кода
+
 class Task4 {
     public static void main(String[] args) {
 
@@ -11,6 +14,7 @@ class Task5 {
     public static void main(String[] args) {
         int x = 10;
         int y = 20;
+        // Придумай для этой переменной более значимое имя
         int c = x + y;
         System.out.println("Sum of x+y =" + c);
     }
@@ -20,6 +24,8 @@ class Task6 {
     public static void main(String[] args) {
 
         double a = 10, b = 20;
+        
+        // Зачем вводил эту переменную? 
         double r = a + b;
         System.out.println("произведение: " + a * b);
     }
@@ -29,15 +35,18 @@ class Task7 {
     public static void main(String[] args) {
 
         int x = 33;
+        // нужны пробелы между: 33 % 2, форматирование кода тебе поможет
         int y = 33%2;
         System.out.println(y);
     }
 }
 class Task8 {
     public static void main(String[] args) {
+        // Переменные именуются с маленькой буквы, helloWorld. Переименуй плиз.
         String HelloWorld = "Мир приветсвует";
         String GoodbayWorld = "Мир попрощается";
 
+        // Ты объявил переменные, но не использовал их. Используй их в выводе на экран
         System.out.println("Мир приветсвует " + "Мир попрощается");
 
     }
