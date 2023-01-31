691f50f443ef997c83bb411406153ecda46f931e
GromGT12 <m.krymov@wp.pl>
2023-01-31 21:13:11 +0100

    31.01.Maks

---
+++ src/main/java/home_work_3/Task9.java
@@ -0,0 +1,24 @@
+package home_work_3;
+
+public class Task9 {
+    public static void main(String[] args) {
+        for (int a = 0; a < 1; a++) {
+            for (int b = 0; b < 1; b++) {
+                System.out.println("* ");
+                System.out.print("*");
+                System.out.println("* ");
+                System.out.println("*** ");
+                System.out.println("****");
+                System.out.println(" ");
+                System.out.println("****");
+                System.out.println("***");
+                System.out.println("**");
+                System.out.println("*");
+
+            }
+        }
+    }
+}
+
+
+
