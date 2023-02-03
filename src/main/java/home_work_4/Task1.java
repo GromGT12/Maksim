77f830f28a3752177f0fe471db5db20ef42f6119
GromGT12 <m.krymov@wp.pl>
2023-02-03 13:26:19 +0100

    Maks 4

---
+++ src/main/java/home_work_4/Task1.java
@@ -0,0 +1,16 @@
+package home_work_4;
+
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the currency");
+        String currency = scanner.nextLine();
+        System.out.println("Enter the sum");
+        int sum = scanner.nextInt();
+        System.out.println("Get uah 800");
+
+    }
+}
+
