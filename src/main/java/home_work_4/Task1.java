067e7da1d465010140de3bd40df553aefdba10c2
Pavel Radkevich <pradkevich@eisgroup.com>
2023-02-03 14:53:36 +0200

    ДЗ 4, первая проверка

--- src/main/java/home_work_4/Task1.java
+++ src/main/java/home_work_4/Task1.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+
+        // Здесь идея немного глубже, программа должна быть динамичной. Как пример - InteractiveSportsman
+        // представь онлайн конвертер - вводишь 10 евро - показывает 400 гривен, вводишь 2- - показывает 800.
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the currency");
         String currency = scanner.nextLine();
