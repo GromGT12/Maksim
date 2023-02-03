688bfdc08f6332b05ba049c27c03ec23e72afbd8
GromGT12 <m.krymov@wp.pl>
2023-02-03 14:46:44 +0100

    Maks 5

--- src/main/java/home_work_4/Task1.java
+++ src/main/java/home_work_4/Task1.java
@@ -4,12 +4,30 @@ import java.util.Scanner;
 
 public class Task1 {
     public static void main(String[] args) {
+
+        int choise;
+        int amount;
+        int euro,hryvnia;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter the currency");
-        String currency = scanner.nextLine();
-        System.out.println("Enter the sum");
-        int sum = scanner.nextInt();
-        System.out.println("Get uah 800");
+        System.out.println("Options:");
+        System.out.println("Enter currency 1: euro");
+        System.out.println("Enter currency 2: hryvnia");
+
+        System.out.println("Choose you options\n");
+        choise = scanner.nextInt();
+        System.out.println("Your number is: " + choise);
+
+        System.out.println("Enter the amount you want to convert:");
+        amount = scanner.nextInt();
+        System.out.println("Your amount is: " + amount);
+
+        euro = amount * 4.70;
+
+        System.out.println(amount + " euro = " + hryvnia + "euro");
+
+        hryvnia = amount * 0.025;
+
+        System.out.println(amount + " hryvnia = " + euro + "euro");
 
     }
 }
