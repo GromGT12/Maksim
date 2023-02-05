9110d511430137a087d46a36e6fd97b316b6ac82
GromGT12 <m.krymov@wp.pl>
2023-02-05 14:46:04 +0100

    Maks Task1

--- src/main/java/home_work_3/Task9.java
+++ src/main/java/home_work_3/Task9.java
@@ -7,8 +7,8 @@ public class Task9 {
         for (int a = 0; a < 1; a++) {
             for (int b = 0; b < 1; b++) {
                 System.out.println("* ");
-                System.out.print("*");
-                System.out.println("* ");
+                System.out.print("");
+                System.out.println("** ");
                 System.out.println("*** ");
                 System.out.println("****");
                 System.out.println(" ");
--- src/main/java/home_work_4/Task1.java
+++ src/main/java/home_work_4/Task1.java
@@ -2,31 +2,43 @@ package home_work_4;
 
 import java.util.Scanner;
 
-class Task1 {
+public class Task1 {
+
     public static void main(String[] args) {
 
-        int choise;
-        double amount;
-        double euro, hryvnia;
 
         Scanner scanner = new Scanner(System.in);
+        double choise;
+        double rateEuro = 0.25;
+        double rateUah = 40;
+
+
         System.out.println("Options:");
         System.out.println("Enter currency 1: euro");
         System.out.println("Enter currency 2: hryvnia");
-        System.out.println("");
+        System.out.println(" ");
         System.out.println("Choose you options\n");
-        choise = scanner.nextInt();
+        choise = scanner.nextDouble();
         System.out.println("Your number is: " + choise);
 
         System.out.println("Enter the amount you want to convert:");
-        amount = scanner.nextInt();
-        System.out.println("Your amount is: " + amount);
+        choise = scanner.nextDouble();
+        System.out.println("Your amount is: " + choise);
 
-        euro = amount *1;
-        System.out.println(amount + " euro = " + hryvnia + "euro");
-        hryvnia = amount * 40.03;
-        System.out.println(amount + " hryvnia = " + euro + "hryvnia");
+        double cyrrency = scanner.nextDouble();
 
+
+        System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - Euro, 2 - Uah, 3 - результат об ошибке");
+        int currency = scanner.nextInt();
+
+        if (currency ==1) {
+            System.out.println("Если выбрали вариант 1: покупаем euro, значит надо choise/rate и вывести результат");
+        } else if (currency == 2) {
+            System.out.println("Если выбрали режим 2: покупаем hryvnia, значит надо amount*rate и вывести результат");
+        } else if (cyrrency == 3) {
+            System.out.println("Если режим 1 и не 2 - выводим сообщение об ошибке");
+        } else {
+            System.out.println("Неизвестная валюта");
+        }
     }
 }
-
