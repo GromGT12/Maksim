2dc2ca950816f542194c55b2304da04d7a04c3f1
GromGT12 <m.krymov@wp.pl>
2023-02-05 16:37:48 +0100

    Homework Task1

--- src/main/java/home_work_4/Task1.java
+++ src/main/java/home_work_4/Task1.java
@@ -6,41 +6,22 @@ public class Task1 {
 
     public static void main(String[] args) {
 
-
         Scanner scanner = new Scanner(System.in);
-        double choise;
-        double rateEuro = 0.25;
-        double rateUah = 40;
+        double rate = 40;
 
-
-        System.out.println("Options:");
-        System.out.println("Enter currency 1: euro");
-        System.out.println("Enter currency 2: hryvnia");
-        System.out.println(" ");
-        System.out.println("Choose you options\n");
-        choise = scanner.nextDouble();
-        System.out.println("Your number is: " + choise);
-
-        System.out.println("Enter the amount you want to convert:");
-
-        choise = scanner.nextDouble();
-        System.out.println("Your amount is: " + choise);
-
-        double cyrrency = scanner.nextDouble();
+        System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - Euro, 2 - Uah,");
+        int mode = scanner.nextInt();
+        System.out.println("Введите сумму");
         double amount = scanner.nextDouble();
-        System.out.println("Your amount is: " + amount);
+        if (mode == 1) {
+            System.out.println("Если выбрали вариант 1: покупаем euro");
+            double result = amount / rate;
+            System.out.println(result + "Euro");
+        } else if (mode == 2) {
+            System.out.println("Если выбрали режим 2: покупаем uah");
+            double result = amount * rate;
+            System.out.println(result);
 
-
-
-        System.out.println("В какую валюту хотите конвертировать? Доступные варианты: 1 - Euro, 2 - Uah, 3 - результат об ошибке");
-        int currency = scanner.nextInt();
-
-        if (currency ==1) {
-            System.out.println("Если выбрали вариант 1: покупаем euro, значит надо choise/rate и вывести результат");
-        } else if (currency == 2) {
-            System.out.println("Если выбрали режим 2: покупаем hryvnia, значит надо amount*rate и вывести результат");
-        } else if (cyrrency == 3) {
-            System.out.println("Если режим 1 и не 2 - выводим сообщение об ошибке");
         } else {
             System.out.println("Неизвестная валюта");
         }
