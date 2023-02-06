05c6fa0f1d7b536c04b42ae58b014613fc48497e
GromGT12 <m.krymov@wp.pl>
2023-02-06 20:14:19 +0100

    Variant 2

--- src/main/java/home_work_4/Task2.java
+++ src/main/java/home_work_4/Task2.java
@@ -5,33 +5,32 @@ import java.util.Scanner;
 class Task2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int enginecapacity;
         double discont = 0.8;
         double rate = 0.25;
         double violationsTrafficrules = 1.2;
         double insurancePrice;
-        boolean accidentСase;
+        boolean accidentCase;
         boolean regularCustomer;
 
         System.out.println("Enter engine size in cm3");
-        int engineCapacity = scanner.nextInt();
-        insurancePrice = engineCapacity * rate;
+        int enginecapacity = scanner.nextInt();
+        insurancePrice = enginecapacity * rate;
 
         System.out.println("Are we insured?? Enter 'true' or 'false':");
         regularCustomer = scanner.nextBoolean();
-        insurancePrice = engineCapacity * rate;
+        insurancePrice = enginecapacity * rate;
 
         if (regularCustomer) {
-            insurancePrice = insurancePrice * 0.8;
+            insurancePrice = insurancePrice * discont;
         }
 
         System.out.println("Were there traffic violations?? Enter 'true' or 'false':");
-        accidentСase = scanner.nextBoolean();
+        accidentCase = scanner.nextBoolean();
 
-        if (accidentСase) {
-            insurancePrice = insurancePrice * 1.2 ;
+        if (accidentCase) {
+            insurancePrice = insurancePrice * violationsTrafficrules;
         }
-        System.out.println("The cost of your policy is " +  insurancePrice + " euros");
+        System.out.println("The cost of your policy is " + insurancePrice + " euros");
     }
 }
 
