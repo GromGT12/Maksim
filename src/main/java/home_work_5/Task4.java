bba79245f0de71c38af96e52c04cfb3d5ac1374a
GromGT12 <m.krymov@wp.pl>
2023-02-07 09:24:19 +0100

    Homework5

---
+++ src/main/java/home_work_5/Task4.java
@@ -0,0 +1,32 @@
+package home_work_5;
+
+import java.util.Scanner;
+
+public class Task4 {
+    public static void main( String[] args ) {
+
+        Scanner in = new Scanner( System.in );
+        int n = in.nextInt();
+        int m = in.nextInt();
+
+        int[][] arr = new int[5][6];
+
+        int i = 0, j = 0;
+        for ( int k = 0; k < n * m; ++k, ++j ) {
+
+            if ( k != 0 && k % m == 0 ) {
+                ++i;
+                j = 0;
+                System.out.println();
+            }
+
+            arr[ i ][ j ] = i * j;
+            System.out.format( "%4d", arr[ i ][ j ] );
+
+        }
+
+    }
+
+}
+
+
