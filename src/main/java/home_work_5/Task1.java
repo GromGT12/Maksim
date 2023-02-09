1928d9239fb2f2f03eddda71c3274ad0fe6b8957
Pavel Radkevich <pradkevich@eisgroup.com>
2023-02-09 18:21:04 +0200

    ДЗ 5, проверка 2

--- src/main/java/home_work_5/Task1.java
+++ src/main/java/home_work_5/Task1.java
@@ -15,6 +15,9 @@ class Task1 {
         System.out.println("Введите наименьшее сисло массива:");
         int numbermin = scanner.nextInt();
         int[] number = new int[]{numbermax, numbermin};
+
+        // здесь ты сортируешь массив number, в котором есть только два элемента, которые ввел пользователь. Нужно
+        // чтобы пользователь ввел три числа и работать с этими числами
         Arrays.sort(number);
 
         int sum = 0;
@@ -23,6 +26,8 @@ class Task1 {
         }
         System.out.println("Сумма: " + sum);
 
+        // а здесь ты ищешь максимальное значение в массиве, который создан в строке 10. его не нужно создавать заранее,
+        // его нужно собрать из чисел, которые ввел пользователь
         int max = getMax(array);
         System.out.println("Maximum Value is: " + max);
 
--- src/main/java/home_work_5/Task4.java
+++ src/main/java/home_work_5/Task4.java
@@ -4,6 +4,7 @@ class Task4 {
 
     public static void main(String[] args) {
 
+        // все хорошо, все работает, но что значит название переменной pif?
         int[] pif = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
         for (int q : pif) {
             for (int w : pif) {
