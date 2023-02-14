c6f8c65c6daf6ecfb77d137c9861fc775ca04f27
GromGT12 <m.krymov@wp.pl>
2023-02-14 17:14:08 +0100

    Home7

--- src/main/java/home_work_7/ArraysForCar.java
+++ src/main/java/home_work_7/ArraysForCar.java
@@ -6,15 +6,11 @@ public class ArraysForCar {
 
     public static void main(String[] args) {
         Car[] Audi = new Car[2];
-
         Audi[0] = new Car("Audi", "A6", 4);
         Audi[1] = new Car("Audi", "A8", 4);
-
-        for (int i = 0; i < 2; i++) {
-                System.out.println(Audi[i]);
 
                 System.out.println(Arrays.toString(Audi));
             }
         }
-    }
+
 
--- src/main/java/home_work_7/GettersSetters.java
+++ src/main/java/home_work_7/GettersSetters.java
@@ -1,10 +1,14 @@
 package home_work_7;
 
-class GettersSettersCar {
+class GettersSetters {
     public static void main(String[] args) {
         Car car = new Car("Audi", "A8", 4);
 
         String brand = car.getBrand();
         System.out.println(car);
+
+        int numbersofdoors = car.getNumberofDoors();
+        System.out.println(" "
+                + "Дверь 1 открыта" + " " + "Дверь два открыта" + " " + "Дверь 3 открыта" + " " + "Двверь 4 открыта");
     }
 }
