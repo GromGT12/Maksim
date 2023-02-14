5293a8768b665086b8fec605123c237fbab2147b
GromGT12 <m.krymov@wp.pl>
2023-02-14 17:14:08 +0100

    Home7

--- src/main/java/home_work_7/GettersSetters.java
+++ src/main/java/home_work_7/GettersSetters.java
@@ -6,5 +6,9 @@ class GettersSettersCar {
 
         String brand = car.getBrand();
         System.out.println(car);
+
+        int numbersofdoors = car.getNumberofDoors();
+        System.out.println(" "
+                + "Дверь 1 открыта" + " " + "Дверь два открыта" + " " + "Дверь 3 открыта" + " " + "Двверь 4 открыта");
     }
 }
