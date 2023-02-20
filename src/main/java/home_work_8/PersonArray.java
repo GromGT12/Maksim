ddca3e70e055731072c6f85003592b5bb999143f
GromGT12 <m.krymov@wp.pl>
2023-02-20 18:23:10 +0100

    Homework8new111

--- src/main/java/home_work_8/PersonArray.java
+++ src/main/java/home_work_8/PersonArray.java
@@ -13,19 +13,21 @@ package home_work_8;
 */
 
 
-public class PersonArray {
-
-    public static void main(String[] args) {
-        Personwihtenum person1 = new Personwihtenum("Dima", "Nowak", 22-11-1990,"Male", "single");
-        Personwihtenum person2 = new Personwihtenum("Diana", "Evseyeva", 22-11-2000,"Female", "single");
-        Personwihtenum person3 = new Personwihtenum("Alina", "Kiriyenko", 22-11-2002,"Female", "single");
-        Personwihtenum person4 = new Personwihtenum("Maks", "Nilolaev", 22-11-1993,"Male", "single");
-
-        Personwihtenum[] personwihtenum = new Personwihtenum[] {person1, person2, person3, person4};
-
-        // Мы хотели тут реализовать глаынй код, но так и не сомгли понять как это сделать. Я уже молчу про остальное
 
 
-    }
-}
+      public enum PersonArray {
+          ;
+
+          String main(String[] args) {
+              Personwihtenum person = new Personwihtenum("Dima", "Nowak", 22 - 11 - 1990, "Male", "single");
+               return person.getFamilyStatus();
+
+               Personwihtenum Personwihtenum = person.getFamilyStatus();
+              if (this.equals(person));
+              System.out.println("Are you 18 years and you single?"  person.equals(PersonFamalyStatus));
+          }
+      }
+
+
+
 
--- src/main/java/home_work_8/Personwihtenum.java
+++ src/main/java/home_work_8/Personwihtenum.java
@@ -1,30 +1,51 @@
 package home_work_8;
 
 import java.util.Objects;
+
 public class Personwihtenum {
     private String name;
     private String surname;
     private int yearOfBirth;
-    private String gender;
-    private String familyStatus;
+    private PersonGender gender;
+    private PersonFamalyStatus familyStatus;
 
-    public Personwihtenum(String name, String surname, int yearOfBirth, String gender, String famalyStatus) {
-        this.name = name;
-        this.surname = surname;
-        this.yearOfBirth = yearOfBirth;
-        this.gender = gender;
-        this.familyStatus = famalyStatus;
+    public String getName() {
+        return name;
     }
-    public String getGender() {
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getYearOfBirth() {
+        return yearOfBirth;
+    }
+
+    public void setYearOfBirth(int yearOfBirth) {
+        this.yearOfBirth = yearOfBirth;
+    }
+
+    public PersonGender getGender() {
         return gender;
     }
-    public void setGender(String gender) {
+
+    public void setGender(PersonGender gender) {
         this.gender = gender;
     }
-    public String getFamilyStatus() {
+
+    public PersonFamalyStatus getFamilyStatus() {
         return familyStatus;
     }
-    public void setFamilyStatus(String familyStatus) {
+
+    public void setFamilyStatus(PersonFamalyStatus familyStatus) {
         this.familyStatus = familyStatus;
     }
 
@@ -33,8 +54,9 @@ public class Personwihtenum {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Personwihtenum that = (Personwihtenum) o;
-        return yearOfBirth == that.yearOfBirth && Objects.equals(name, that.name) && Objects.equals(surname, that.surname) && Objects.equals(gender, that.gender) && Objects.equals(familyStatus, that.familyStatus);
+        return yearOfBirth == that.yearOfBirth && Objects.equals(name, that.name) && Objects.equals(surname, that.surname) && gender == that.gender && familyStatus == that.familyStatus;
     }
+
     @Override
     public int hashCode() {
         return Objects.hash(name, surname, yearOfBirth, gender, familyStatus);
@@ -42,15 +64,24 @@ public class Personwihtenum {
 
     @Override
     public String toString() {
-        return "PersonEnum{" +
+        return "Personwihtenum{" +
                 "name='" + name + '\'' +
                 ", surname='" + surname + '\'' +
                 ", yearOfBirth=" + yearOfBirth +
-                ", gender='" + gender + '\'' +
-                ", famalyStatus='" + familyStatus + '\'' +
+                ", gender=" + gender +
+                ", familyStatus=" + familyStatus +
                 '}';
     }
+
+    public Personwihtenum(String name, String surname, int yearOfBirth, PersonGender gender, PersonFamalyStatus familyStatus) {
+        this.name = name;
+        this.surname = surname;
+        this.yearOfBirth = yearOfBirth;
+        this.gender = gender;
+        this.familyStatus = familyStatus;
+        {
+
+        }
+
+    }
 }
-
-
-
