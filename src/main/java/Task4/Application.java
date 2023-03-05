fe2c18d79b97b28e269afab0f02bc1e2b106128f
GromGT12 <m.krymov@wp.pl>
2023-03-05 11:42:42 +0100

    Задание 11 и попарвки Car

--- src/main/java/Task4/Application.java
+++ src/main/java/Task4/Application.java
@@ -4,9 +4,7 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Application {
     public static void main(String[] args) {
-        boolean notBlank = StringUtils.isNotBlank("");
         String reverse = StringUtils.reverse("tod dot");
-
-        System.out.println(reverse);
+        System.out.println(reverse.equals(new StringBuilder(reverse).reverse().toString()));
     }
 }
