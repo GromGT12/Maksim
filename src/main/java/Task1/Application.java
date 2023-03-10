8fa3d6604e903ccf7b90ab51bf877dcca7f27279
GromGT12 <m.krymov@wp.pl>
2023-03-10 22:57:56 +0100

    Maks1

--- src/main/java/Task1/Application.java
+++ src/main/java/Task1/Application.java
@@ -1,6 +1,6 @@
 package Task1;
 
-public class ArrayNumber {
+public class Application {
     public static void main(String[] args) {
         int[] Number = new int[]{3, 7, 9, 21, 14, 15};
         for (int i = 0; i <Number.length; i++) {
