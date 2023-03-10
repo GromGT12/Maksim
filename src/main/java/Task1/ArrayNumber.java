af115df806a946e92c42e38d1135f075c5f4305a
GromGT12 <m.krymov@wp.pl>
2023-03-10 22:25:27 +0100

    HomeWork13

---
+++ src/main/java/Task1/ArrayNumber.java
@@ -0,0 +1,21 @@
+package Task1;
+
+public class ArrayNumber {
+    public static void main(String[] args) {
+        int[] Number = new int[]{3, 7, 9, 21, 14, 15};
+        for (int i = 0; i <Number.length; i++) {
+            {
+                try {
+                System.out.println(100/Number[9]);
+                }catch (ArithmeticException exception) {
+                    System.out.println("При обработке элемента №9 возникла ошибка: " + exception.getMessage());
+                }
+
+                }
+
+        }
+    }
+}
+
+
+
