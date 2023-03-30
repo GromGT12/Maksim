b8f717295d54578fbc5f3a7b26b4c990f95a30d5
GromGT12 <m.krymov@wp.pl>
2023-03-30 13:43:31 +0200

    Maks 30.03

--- src/main/java/home_work_18/Task2/ListNumbers.java
+++ src/main/java/home_work_18/Task2/ListNumbers.java
@@ -1,5 +1,8 @@
 package home_work_18.Task2;
 
+import java.util.ArrayList;
+import java.util.Iterator;
+
 /*
 Создайте целочисленный ArrayList. Заполните его 100 элементами,
 добавляя каждый новый элемент в начало списка и выведите на экран.
@@ -7,6 +10,21 @@ package home_work_18.Task2;
  */
 public class ListNumbers {
     public static void main(String[] args) {
-
+        ArrayList<Integer> list = new ArrayList<>();
+        int elements = 100;
+        for (int i = 0; i <= elements; i++) {
+            list.add(0, i);
+        }
+        System.out.println(elements);
+        Iterator<Integer> iterator = list.iterator();
+        while (iterator.hasNext()) {
+            Integer nextInteger = iterator.next();
+            if (nextInteger % 2 == 0) {
+                iterator.remove();
+            }
+        }
+        System.out.println(list);
     }
 }
+
+
