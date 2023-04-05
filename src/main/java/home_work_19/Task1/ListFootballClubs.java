8ce91b6b4783e6bf998a04398012a3da7df5984f
GromGT12 <m.krymov@wp.pl>
2023-04-05 16:58:53 +0200

    Max 05.04

--- src/main/java/home_work_19/Task1/ListFootballClubs.java
+++ src/main/java/home_work_19/Task1/ListFootballClubs.java
@@ -2,14 +2,14 @@ package home_work_19.Task1;
 
 import java.util.*;
 
-// название класса лучше бы уточнить, не совсем подходит Exception
 public class ListFootballClubs {
     public static void main(String[] args) {
         FootballClub footballClub = new FootballClub("Chelsea", "England", "London");
-        FootballClub footballClub1 = new FootballClub("Real Madrid", "Spain", "Madrid");
-        FootballClub footballClub2 = new FootballClub("Borussia Dortmund", "Germany", "Dortmut");
+        FootballClub footballClub1 = new FootballClub("Red Bull Lepzig", "Austria", "Viena");
+        FootballClub footballClub2 = new FootballClub("Real Madrid", "Spain", "Madrid");
+        FootballClub footballClub3 = new FootballClub("Borussia Dortmund", "Germany", "Dortmut");
 
-        List<FootballClub> footballclubsList = new ArrayList<>(List.of(footballClub, footballClub1, footballClub2));
+        List<FootballClub> footballclubsList = new ArrayList<>(List.of(footballClub, footballClub1, footballClub2, footballClub3));
 
         // В условии задачи нам нужно было вывести только страны, но ты сгруппировал клубы по странам и эту инфу можно было
         // бы использовать где-то - пускай остается так
@@ -33,7 +33,7 @@ public class ListFootballClubs {
         System.out.println("Уникальные страны :" + country);
     }
     private static Set<String> findUniqueCountrys(List<FootballClub> footballclubsList) {
-        Set<String> countryClub = new HashSet<>();
+        Set<String> countryClub = new TreeSet<>();
         for(FootballClub footballClub:footballclubsList) {
             countryClub.add(footballClub.getCountry());
         }
