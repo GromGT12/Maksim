e275dafbb063baa620994101f104289cfcbd3338
GromGT12 <m.krymov@wp.pl>
2023-05-10 23:28:49 +0200

    Maks 10.05

--- src/main/java/home_work_28/Task2/AdverstisementJson.java
+++ src/main/java/home_work_28/Task2/AdverstisementJson.java
@@ -3,31 +3,35 @@ package home_work_28.Task2;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.List;
+import java.util.stream.Stream;
 
 public class AdverstisementJson {
     public static void main(String[] args) throws IOException {
 
         ObjectMapper objectMapper = new ObjectMapper();
 
-       InputStream resource = ClassLoader.getSystemResourceAsStream("Task2/Advertisement.Json");
-        List<JacksonAdvertisement> filterCar = objectMapper.readValue(resource, new TypeReference<>() {
+        InputStream resource = ClassLoader.getSystemResourceAsStream("Task2/Advertisement.Json");
+        List<JacksonAdvertisement> filterCity = objectMapper.readValue(resource, new TypeReference<>() {
         });
-        System.out.println(filterCar);
+        Stream<JacksonAdvertisement> jacksonAdvertisement = filterCity.stream()
+                .filter(jacksonAdvertisement1 -> !"Минск".equalsIgnoreCase(jacksonAdvertisement1.getCity()));
+        jacksonAdvertisement.forEach(System.out::println);
+
+        String record = objectMapper.writeValueAsString(jacksonAdvertisement);
+        System.out.println(record);
+
+        File file = new File("src/main/java/home_work_28/Task2/cars_not_from_minsk.json");
+        objectMapper.writeValue(file, jacksonAdvertisement);
+
+
         // до этого момента код работает, сделайте оставшуюся часть
         /*
         Отфильтруйте список, оставив обявления, где город - НЕ Минск. Сериализуте результат в строку, запишите ее
 в файл cars_not_from_minsk.json
          */
-
-
-        // этот код я закоментил, тк он не будет работать и он по задаче не нужен
-        /*resource = ClassLoader.getSystemResourceAsStream("Task2/Advertisement.Json");
-        Car car = objectMapper.readValue(resource, Car.class);
-
-        System.out.println(car);*/
-
     }
 }
