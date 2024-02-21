fdeac9cd16de184736510a24166683a3ad5d9016
Maks <122127608+GromGT12@users.noreply.github.com>
2024-02-21 22:00:36 +0100

    Coding

--- src/main/java/by_practice/tasks/StreamAPI.java
+++ src/main/java/by_practice/tasks/StreamAPI.java
@@ -16,7 +16,7 @@ public class StreamAPI {
 
         System.out.println("Выводим не паралелный поток" + sumNotParralel);
 
-        List<Integer> numbers2 = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        List<Integer> asList = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // Создание параллельного стрима
         Stream<Integer> parallelStream = numbers.parallelStream();
